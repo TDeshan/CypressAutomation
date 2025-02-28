7e2c0dba71601874ae683090ff1fda6bc9db5a82
TDeshan <ethirial@gmail.com>
2025-02-28 13:21:01 +0530

    updated by 31

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,5 @@
 
-/*
+
 const { defineConfig } = require("cypress");
 
 
@@ -16,28 +16,8 @@ module.exports = defineConfig({
   
 });
 
-*/
 
-const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
-  reporter: "cypress-multi-reporters",
-  reporterOptions: {
-    reporterEnabled: "mochawesome, mocha-junit-reporter",
-    mochawesomeReporterOptions: {
-      reportDir: "cypress/reports/mochawesome",
-      overwrite: false,
-      html: false,
-      json: true
-    },
-    mochaJunitReporterReporterOptions: {
-      mochaFile: "cypress/reports/junit/test-results.xml"
-    }
-  },
-  env: {
-    cucumberJsonReport: "cypress/reports/cucumber/cucumber-report.json"
-  }
-});
 
 
 
